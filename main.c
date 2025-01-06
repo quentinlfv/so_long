43ee14cf975288a14ee1fb8c63de66ea60735fbe
Quentin Lefevre <quelefev@paul-f4Ar7s4.clusters.42paris.fr>
2025-01-06 18:17:25 +0100

    parsing working

--- main.c
+++ main.c
@@ -10,56 +10,100 @@ int	main(int av, char **ac)
 	fd = open(ac[1], O_RDONLY);
 	if (!fd)
 		return (0);
-	get_map(fd, &map);
+	map = get_map(fd, map);
 	close(fd);
 	print_map(map);
+	free_tab(map);
+	return (0);
 }
 
-void	get_map(int fd, char ***map)
+char	**get_map(int fd, char **map)
 {
 	char	*line;
 	char	**tmp;
 
 	tmp = NULL;
 	line = get_next_line(fd);
-	*map = ft_calloc(ft_strlen(line) + 1, sizeof(char));
+	map = malloc(sizeof(char *) * (1 + 1));
 	if (!map)
-		return ;
-	printf("%s\n", line);
-	ft_strcpy(*map[0], line);
-	free(line);
-	while (line)
+		return (NULL);
+	map[1] = NULL;
+	map[0] = malloc(sizeof(char) * (ft_strlen(line) + 1));
+	if (!*map)
+		return (NULL);
+	ft_strcpy(map[0], line);
+	while (line != NULL)
 	{
-		line = get_next_line(fd);
-		tmp = copy_map_to_tmp(map, ft_strlen(line));
-		*map = copy_tmp_to_map(tmp, line, ft_strlen(line));
 		free(line);
+		line = get_next_line(fd);
+		if (line != NULL)
+		{
+			tmp = copy_tab_to_tab(map, NULL, ft_strlen(line));
+			map = copy_tab_to_tab(tmp, line, ft_strlen(line));
+		}
 	}
+	return (map);
 }	
 
+char	**copy_tab_to_tab(char **tab_src,char *line, int len)
+{
+	char	**tab_dest;
+	int	i;
 
-char	**copy_map_to_tmp(char ***map, int len)
+	i = 0;
+	while (tab_src[i])
+		i++;
+	if (line != NULL)
+		tab_dest = malloc(sizeof(char *) * (i + 2));
+	else
+		tab_dest = malloc(sizeof(char *) * (i + 1));
+	if (!tab_dest)
+		return (NULL);
+	i = 0;
+	while (tab_src[i] != NULL)
+	{
+		tab_dest[i] = malloc(sizeof(char) * (len + 1));
+		if (!tab_dest[i])
+			return (NULL);
+		ft_strcpy(tab_dest[i], tab_src[i]);
+		i++;
+	}
+	if (line != NULL)
+	{
+		tab_dest[i] = malloc(sizeof(char) * (len + 1));
+		if (!tab_dest[i])
+			return (NULL);
+		ft_strcpy(tab_dest[i], line);
+		i++;
+	}
+	tab_dest[i] = NULL;
+	free_tab(tab_src);
+	return (tab_dest);
+}
+
+char	**copy_map_to_tmp(char **map, int len)
 {
 	char	**tmp;
 	int	i;
 
 	i = 0;
-	len = ft_strlen(*map[i]);
-	while (*map[i] != NULL)
+	len = ft_strlen(map[i]);
+	while (map[i] != NULL)
 		i++;
-	tmp = malloc(sizeof(char *) * i + 1);
+	tmp = malloc(sizeof(char *) * (i + 1));
 	if (!tmp)
 		return (NULL);
 	tmp[i] = NULL;
 	i = 0;
-	while (tmp[i] != NULL)
+	while (map[i] != NULL)
 	{
-		tmp[i] = malloc(sizeof(char) * len + 1);
+		tmp[i] = malloc(sizeof(char) * (len + 1));
 		if (!tmp[i])
 			return (NULL);
-		ft_strcpy(*map[i], tmp[i]);
+		ft_strcpy(tmp[i], map[i]);
+		i++;
 	}
-	free_tab(*map);
+	free_tab(map);
 	return (tmp);
 }
 
@@ -71,21 +115,23 @@ char	**copy_tmp_to_map(char **tmp, char *line, int len)
 	i = 0;
 	while (tmp[i] != NULL)
 		i++;
-	map = malloc(sizeof(char *) * i + 2);
+	map = malloc(sizeof(char *) * (i + 2));
 	if (!map)
 		return (NULL);
-	map[i] = malloc(sizeof(char) * len + 1);
+	i = 0;
+	while (tmp[i] != NULL)
+	{
+		map[i] = ft_calloc(len + 1, sizeof(char));
+		if (!map[i])
+			return (NULL);
+		ft_strcpy(map[i], tmp[i]);
+		i++;
+	}
+	map[i] = ft_calloc(len + 1, sizeof(char));
 	if (!map[i])
 		return (NULL);
 	ft_strcpy(map[i], line);
-	i++;
-	while (map[i] != NULL)
-	{
-		map[i] = malloc(sizeof(char) * len + 1);
-		if (!map[i])
-			return (NULL);
-		ft_strcpy(map[i], tmp[i -1]);
-	}
+	map[i + 1] = NULL;
 	free_tab(tmp);
 	return (map);
 }
@@ -100,7 +146,7 @@ void	free_tab(char **tab)
 		free(tab[i]);
 		i++;
 	}
-	free(tab[i]);
+	free(tab);
 }
 
 void	print_map(char **map)
--- so_long.h
+++ so_long.h
@@ -3,8 +3,9 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
-void	get_map(int fd, char ***map);
-char	**copy_map_to_tmp(char ***map, int len);
+char	**get_map(int fd, char **map);
+char	**copy_tab_to_tab(char **tab_src,char *line, int len);
+char	**copy_map_to_tmp(char **map, int len);
 char	**copy_tmp_to_map(char **tmp, char *line, int len);
 void	free_tab(char **tab);
 char	*ft_strcpy(char *dest, char *src);
